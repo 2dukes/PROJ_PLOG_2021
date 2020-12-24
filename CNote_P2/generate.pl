aab1314051dddd52a64e954be2e33dc504679b5a
2dukes <ruipinto02@hotmail.com>
2020-12-24 02:10:04 +0000

    Added missing file.

---
+++ CNote_P2/generate.pl
@@ -0,0 +1,28 @@
+generateRow(0, Result, Result) :- !.
+generateRow(M, Aux, Result) :-
+    random(1, 10, Random),
+    append(Aux, [Random], NewAux),
+    NewM is M - 1,
+    generateRow(NewM, NewAux, Result).
+
+geneRandomPuzzleLines(0, _, Result, Result) :- !.
+geneRandomPuzzleLines(N, M, Aux, Result) :-
+    generateRow(M, [], ResultRow),
+    append(Aux, [ResultRow], NewAux),
+    NewN is N - 1,
+    geneRandomPuzzleLines(NewN, M, NewAux, Result).
+
+printR(success, true).
+printR(_, false).
+
+% [[1, 2], [3, 4]]
+generateRandomPuzzle(N, M) :- 
+    repeat, 
+    geneRandomPuzzleLines(N, M, [], InputPuzzle), % Input Grid
+    generateGrid(N-M, [], DynamicGrid, 'FALSE'), % Generate Dynamic List
+    generateGrid(N-M, [], DigitGrid, 'FALSE'), % Generate Digit List
+    
+    cNote(InputPuzzle, DynamicGrid, DigitGrid, ResultGrid, Flag, 500),
+    printR(Flag, PrintResult),
+    finalCNote(Flag, InputPuzzle, ResultGrid, N, M, PrintResult).
+    % write(ResultPuzzle).
