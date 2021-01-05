52bb07ee77c1854ead7627f0fe0d3ff326db25a9
2dukes <ruipinto02@hotmail.com>
2021-01-05 12:44:41 +0000

    Revert "Just a Nitpick"
    This reverts commit 4b3d3d291ecfcd44d7fc9040c812c868a15ddc5d.

--- CNote_P2/src/cnote.pl
+++ CNote_P2/src/cnote.pl
@@ -63,7 +63,7 @@ cNote(_, _, _, _, nosolutions, _, _).
 
 % Labeling (solver)
 labelCNote(ResultGrid, _, _, solvePuzzle) :-
-    labeling([ffc], ResultGrid).
+    labeling([ff, ffc], ResultGrid).
 
 % Labeling (generator)
 labelCNote(ResultGrid, Timeout, Flag, generatePuzzle) :-
